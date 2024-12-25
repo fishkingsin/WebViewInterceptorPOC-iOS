8c8e0b7c36738e92c06517c3775daa22d9cf9f51
James Kong <fishkingsin@gmail.com>
2024-12-25 18:10:52 +0800

    Initial Commit

---
+++ WebViewInterceptorPOC/WebViewInterceptorPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewInterceptorPOCApp.swift
+//  WebViewInterceptorPOC
+//
+//  Created by James Kong on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WebViewInterceptorPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebViewInterceptorPOCTests/WebViewInterceptorPOCTests.swift
@@ -0,0 +1,16 @@
+//
+//  WebViewInterceptorPOCTests.swift
+//  WebViewInterceptorPOCTests
+//
+//  Created by James Kong on 25/12/2024.
+//
+
+import Testing
+
+struct WebViewInterceptorPOCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
